751dc3ea2830ace180ca64571fdd8f1bbe1a9362
six-two <git@six-two.dev>
2025-04-26 18:22:18 +0200

    Added grouping to inline badges

--- src/mkdocs_badges/badge_handler.py
+++ src/mkdocs_badges/badge_handler.py
@@ -19,8 +19,8 @@ STRIP_SUBDOMAINS = [
 # > Sadly I can not put the HTML in directly, because then the reference link markdown would not be parsed.
 # > So I insert a placeholder, let the markdown parser convert the reference links, and then replace the placeholders with the true HTML
 # Actually the problem described above can be circumvented by simply using a span instead of a div
-BADGE_GROUP_START = '<span class="badge-group">\n'
-BADGE_GROUP_END = '\n</span>'
+BADGE_GROUP_START = '<span class="badge-group">'
+BADGE_GROUP_END = '</span>'
 
 
 def replace_badges(file_name: str, markdown: str, badge_separator: str, badge_table_separator: str, *args) -> str:
@@ -28,33 +28,99 @@ def replace_badges(file_name: str, markdown: str, badge_separator: str, badge_ta
 
     parser_result_list = FileParser(file_name, lines, badge_separator, badge_table_separator).process()
     if parser_result_list:
-        # Replace the lines with the rendered badges
+        # This is to track entirely replaced lines
         replaced_line_indices = []
+
+        replacements_by_line_index: dict[int,list[ParserResultEntry]] = {}
         for entry in parser_result_list:
             try:
-                badge_html = format_badge(entry, *args)
-                # Indent it a bit to make debugging it easier when viewing the page's source
+                replacements_by_line_index[entry.line_index].append(entry)
+            except KeyError:
+                replacements_by_line_index[entry.line_index] = [entry]
 
-                # depending on where the badge is from, we need to escape some characters (like '|' in tables)
-                for old, new in entry.replace_characters_before_html_output:
-                    badge_html = badge_html.replace(old, new)
+        # We need this mess if we want to group inline badges. Unless someone has a simpler idea
+        for line_number, entry_list in replacements_by_line_index.items():
+            # lets assume, that all badges are in the correct order
+            line = lines[line_number]
+            if len(entry_list) == 1:
+                entry = entry_list[0]
+                try:
+                    badge_html = format_badge(entry, *args)
+                    # Indent it a bit to make debugging it easier when viewing the page's source
 
-                if entry.only_replace_substring:
-                    # Replace only part of the line (for table cells)
-                    old_line = lines[entry.line_index]
-                    new_line = old_line.replace(entry.only_replace_substring, badge_html)
-                    if old_line != new_line:
-                        lines[entry.line_index] = new_line
+                    # depending on where the badge is from, we need to escape some characters (like '|' in tables)
+                    for old, new in entry.replace_characters_before_html_output:
+                        badge_html = badge_html.replace(old, new)
+
+                    if entry.only_replace_substring:
+                        # Replace only part of the line (for table cells)
+                        old_line = lines[entry.line_index]
+                        new_line = old_line.replace(entry.only_replace_substring, badge_html)
+                        if old_line != new_line:
+                            lines[entry.line_index] = new_line
+                        else:
+                            warning_for_entry(entry, f"Replacing '{entry.only_replace_substring}' in line '{old_line}' failed")
                     else:
-                        warning_for_entry(entry, f"Replacing '{entry.only_replace_substring}' in line '{old_line}' failed")
-                else:
-                    # Replaced the entire line
-                    lines[entry.line_index] = f"\t{badge_html}"
-                    replaced_line_indices.append(entry.line_index)
-            except BadgeException as error:
-                warning_for_entry(entry, f"Processing error: {error}")
+                        # Replaced the entire line
+                        lines[entry.line_index] = f"\t{badge_html}"
+                        replaced_line_indices.append(entry.line_index)
+                except BadgeException as error:
+                    warning_for_entry(entry, f"Processing error: {error}")
+            else:
+                badge_groups = []
+                badge_group_start = -1
+                last_badge_end = -1
+                badge_group_size = 0
+                for entry in entry_list:
+                    try:
+                        badge_html = format_badge(entry, *args)
+                        # depending on where the badge is from, we need to escape some characters (like '|' in tables)
+                        for old, new in entry.replace_characters_before_html_output:
+                            badge_html = badge_html.replace(old, new)
 
-        # Surround each group of badges with the container placeholders (required for layout)
+                        if entry.only_replace_substring:
+                            try:
+                                current_start = line.index(entry.only_replace_substring)
+                            except ValueError:
+                                # Handle cases where we did not find the substring
+                                warning_for_entry(entry, f"badge_handler::replace_badges: Failed to find '{entry.only_replace_substring}' in line:\n{line}\nBefore any replacements the line was:\n{lines[line_number]}")
+                                continue
+
+                            # Check if there is only white space between the end of the last badge and the start of this one
+                            if badge_group_start == -1 or line[last_badge_end:current_start].strip():
+                                # Not just white space, end the previous group
+                                if badge_group_size > 1:
+                                    badge_groups.append((badge_group_start, last_badge_end))
+
+                                # Start a new badge group
+                                badge_group_start = current_start
+                                badge_group_size = 1
+                            else:
+                                # Continue the current group
+                                badge_group_size += 1
+
+                            last_badge_end = current_start + len(badge_html)
+                            # replace original badge syntax with HTML
+                            end_index_in_original_line = current_start + len(entry.only_replace_substring)
+                            line = line[:current_start] + badge_html + line[end_index_in_original_line:]
+                        else:
+                            warning_for_entry(entry, "BUG: There are multiple badges in a line, but it also claims that the whole line is a badge. This is mutually exclusive.")
+                    except BadgeException as error:
+                        warning_for_entry(entry, f"Processing error: {error}")
+                
+                # After the last badge, close any open groups
+                if badge_group_size > 1:
+                    badge_groups.append((badge_group_start, last_badge_end))
+
+                # Handle in reverse order, so that the indices remain correct
+                for group_start, group_end in reversed(badge_groups):
+                    # print(f"Debug: Inline badge group: line {line_number}, start {group_start}, end {group_end}")
+                    line = line[:group_start] + BADGE_GROUP_START + line[group_start:group_end] + BADGE_GROUP_END + line[group_end:]
+
+                # Finally store the modified line
+                lines[line_number] = line
+
+        # Surround each group of badges (whole lines) with the container placeholders (required for layout)
         replaced_line_indices = list(sorted(replaced_line_indices))
         last_i = len(replaced_line_indices) - 1
         for i, line in enumerate(replaced_line_indices):
