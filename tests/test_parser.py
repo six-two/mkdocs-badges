013d3afc7ebfd2f5075ad93b2350abb6ad268953
six-two <git@six-two.dev>
2025-08-24 19:58:31 +0200

    build(tests): Fixed test

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -5,23 +5,31 @@ from mkdocs_badges.parser import BadgeException, FileParser, ParsedBadge, Parser
 BADGE_SEPARATOR = "|"
 BADGE_TABLE_SEPARATOR = "^"
 FILE_NAME = "TestFileParser"
+INLINE_BADGE_START = "["
+INLINE_BADGE_END = "]"
+
+def parse_lines(lines: list[str]):
+    return FileParser(FILE_NAME, lines, BADGE_SEPARATOR, BADGE_TABLE_SEPARATOR, INLINE_BADGE_START, INLINE_BADGE_END).process()
+
 
 class TestFileParser(unittest.TestCase):
+
     def test_badge_simple(self):
-        results = FileParser(FILE_NAME, ["|title|value|"], BADGE_SEPARATOR, BADGE_TABLE_SEPARATOR).process()
+        results = parse_lines(["|title|value|"])
 
         expected_results = [
             ParserResultEntry(
                 file_name=FILE_NAME,
                 line_index=0,
-                parsed_badge=ParsedBadge(None, "title", "value", None, None, None, []),
                 only_replace_substring=None,
+                parsed_badge=ParsedBadge(None, "title", "value", None, None, None, []),
+                replace_characters_before_html_output=[],
             )
         ]
         self.assertEqual(results, expected_results)
 
     def test_badge_install(self):
-        results = FileParser(FILE_NAME, ["I|pypi|mkdocs-badges|"], BADGE_SEPARATOR, BADGE_TABLE_SEPARATOR).process()
+        results = parse_lines(["I|pypi|mkdocs-badges|"])
 
         expected_results = [
             ParserResultEntry(
@@ -29,12 +37,13 @@ class TestFileParser(unittest.TestCase):
                 line_index=0,
                 parsed_badge=ParsedBadge("I", "pypi", "mkdocs-badges", None, None, None, []),
                 only_replace_substring=None,
+                replace_characters_before_html_output=[],
             )
         ]
         self.assertEqual(results, expected_results)
 
     def test_badge_type_unknown(self):
-        results = FileParser(FILE_NAME, ["X|title|value|"], BADGE_SEPARATOR, BADGE_TABLE_SEPARATOR).process()
+        results = parse_lines(["X|title|value|"])
 
         expected_results = [
             ParserResultEntry(
@@ -42,30 +51,31 @@ class TestFileParser(unittest.TestCase):
                 line_index=0,
                 parsed_badge=ParsedBadge("X", "title", "value", None, None, None, []),
                 only_replace_substring=None,
+                replace_characters_before_html_output=[],
             )
         ]
         self.assertEqual(results, expected_results)
 
     def test_badge_no_trailing_pipe(self):
-        results = FileParser(FILE_NAME, ["X|title|value"], BADGE_SEPARATOR, BADGE_TABLE_SEPARATOR).process()
+        results = parse_lines(["X|title|value"])
 
         expected_results = []
         self.assertEqual(results, expected_results)
 
     def test_badge_table_header(self):
-        results = FileParser(FILE_NAME, ["|---|---|"], BADGE_SEPARATOR, BADGE_TABLE_SEPARATOR).process()
+        results = parse_lines(["|---|---|"])
 
         expected_results = []
         self.assertEqual(results, expected_results)
 
     def test_badge_in_table(self):
-        results = FileParser(FILE_NAME, ["|A|B|", "|---|---|", "|a|b|", "|c|d|"], BADGE_SEPARATOR, BADGE_TABLE_SEPARATOR).process()
+        results = parse_lines(["|A|B|", "|---|---|", "|a|b|", "|c|d|"])
 
         expected_results = []
         self.assertEqual(results, expected_results)
 
     def test_badge_after_table(self):
-        results = FileParser(FILE_NAME, ["|A|B|", "|---|---|", "|a|b|", "|c|d|", "", "|title|value|"], BADGE_SEPARATOR, BADGE_TABLE_SEPARATOR).process()
+        results = parse_lines(["|A|B|", "|---|---|", "|a|b|", "|c|d|", "", "|title|value|"])
 
         expected_results = [
             ParserResultEntry(
@@ -73,19 +83,20 @@ class TestFileParser(unittest.TestCase):
                 line_index=5,
                 parsed_badge=ParsedBadge("", "title", "value", None, None, None, []),
                 only_replace_substring=None,
+                replace_characters_before_html_output=[],
             )
         ]
         self.assertEqual(results, expected_results)
 
 
     def test_badge_type_too_long(self):
-        results = FileParser(FILE_NAME, ["XYZ|title|value|"], BADGE_SEPARATOR, BADGE_TABLE_SEPARATOR).process()
+        results = parse_lines(["XYZ|title|value|"])
 
         expected_results = []
         self.assertEqual(results, expected_results)
 
     def test_badge_with_escaped_pipes(self):
-        results = FileParser(FILE_NAME, [r"|title\|\\|value|"], BADGE_SEPARATOR, BADGE_TABLE_SEPARATOR).process()
+        results = parse_lines([r"|title\|\\|value|"])
 
         expected_results = [
             ParserResultEntry(
@@ -93,6 +104,7 @@ class TestFileParser(unittest.TestCase):
                 line_index=0,
                 parsed_badge=ParsedBadge("", "title|\\", "value", None, None, None, []),
                 only_replace_substring=None,
+                replace_characters_before_html_output=[],
             )
         ]
         self.assertEqual(results, expected_results)
